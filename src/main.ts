a52660dda912716b07ba2f8ce574215d9ea5aabf
vamshikrishna <vamshi@fello.in>
2024-07-03 17:53:06 +0530

    updated cors urkl

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // Allow requests from this origin
+    origin: 'https://onedrive-client.onrender.com', // Allow requests from this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
